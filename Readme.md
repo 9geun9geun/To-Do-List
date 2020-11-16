ffe167e8bee0311fa42031d5d1a30ef5bff9a2b9
Keun Mo <rnrmsah.koo@gmail.com>
2020-11-17 02:58:10 +1300

    new files

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Node Todo App
+
+A Node app built with MongoDB and Angular. For demonstration purposes and a tutorial.
---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+    remoteUrl : 'mongodb://cosmosdb-keunmo:9ycLR1eUi7FJArU4V0lV0rx3sHGBivynU6P8tn26JGJGuJw0Yt4PLJqX6QGz04mc3yuZ5TqMabtrMWxaLYrRrw==@cosmosdb-keunmo.documents.azure.com:10255/?ssl=true&replicaSet=globaldb',
+
+
+    localUrl: 'mongodb://localhost/meanstacktutorials'
+};
